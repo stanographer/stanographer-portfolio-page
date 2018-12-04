a090c630695e1b497b014f63246e06ff4c87f2f4
stanographer <sakai.stanley@gmail.com>
2018-12-04 02:07:07 -0500

    Defer images.

--- js/main.js
+++ js/main.js
@@ -356,4 +356,16 @@
     return false;
   });
 
+  // Defer images.
+  function init() {
+    var imgDefer = document.getElementsByTagName('img');
+    for (var i = 0; i < imgDefer.length; i++) {
+      if (imgDefer[i].getAttribute('data-src')) {
+        imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
+      }
+    }
+  }
+
+  window.onload = init;
+
 })(jQuery);
