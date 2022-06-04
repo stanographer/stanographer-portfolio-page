53afe002d4fa326552f01100b4d73f94100b907a
Stanley Sakai <stanley@stanographer.com>
2022-06-04 17:55:29 -0400

    imagems

--- dist/index.788a9e14.js
+++ dist/index.788a9e14.js
@@ -506,15 +506,23 @@ function hmrAcceptRun(bundle, id) {
 const elementos = Array.from(document.querySelector("section").children);
 const interpreter = document.querySelector(".interpreter--text");
 const langTag = document.querySelector(".interpreter--lang-tag");
+const icons = {
+    es: "spain",
+    en: "uk",
+    nl: "netherlands",
+    kr: "south-korea",
+    pt: "portugal"
+};
+const getIconUrl = (langCode)=>`img/${icons[langCode]}.png`;
 elementos.forEach((elemento)=>{
-    console.log(elemento);
     elemento.addEventListener("mouseover", (el)=>{
-        // console.log();
         const lang = el.target.dataset.tran.split("|")[0];
         const tran = el.target.dataset.tran.split("|")[1];
+        const img = document.createElement("img");
         el.target.classList.add("traducido");
         interpreter.innerHTML = tran || "";
-        langTag.innerHTML = lang || "";
+        img.src = new URL(getIconUrl(lang) || "", "file:///script.js");
+        langTag.appendChild(img);
     }, true);
     elemento.addEventListener("mouseout", (el)=>{
         el.target.classList.remove("traducido");
--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 const elementos = Array.from(document.querySelector('section').children);
 const interpreter = document.querySelector('.interpreter--text');
 const langTag = document.querySelector('.interpreter--lang-tag');
+const icons = {
+  es: 'spain',
+  en: 'uk',
+  nl: 'netherlands',
+  kr: 'south-korea',
+  pt: 'portugal',
+};
+
+const getIconUrl = langCode => `img/${icons[langCode]}.png`;
 
 elementos.forEach(elemento => {
-  console.log(elemento)
   elemento.addEventListener('mouseover', el => {
-    // console.log();
     const lang = el.target.dataset.tran.split('|')[0];
     const tran = el.target.dataset.tran.split('|')[1];
-    el.target.classList.add('traducido');
+    const img = document.createElement('img');
 
+    el.target.classList.add('traducido');
     interpreter.innerHTML = tran || '';
-    langTag.innerHTML = lang || '';
+    
+    img.src = new URL(getIconUrl(lang) || '', import.meta.url);
+    langTag.appendChild(img)
   }, true);
 
   elemento.addEventListener('mouseout', el => {
     el.target.classList.remove('traducido');
   }, true);
 });
+
