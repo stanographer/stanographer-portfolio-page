6105ba99490e60c950013a60e52ed6cb0c3d0a8a
Stanley Sakai <stanley@stanographer.com>
2022-06-04 23:14:48 -0400

    looks greaat

--- dist/index.788a9e14.js
+++ dist/index.788a9e14.js
@@ -506,6 +506,7 @@ function hmrAcceptRun(bundle, id) {
 const elementos = Array.from(document.querySelector("section").children);
 const interpreter = document.querySelector(".interpreter--text");
 const langTag = document.querySelector(".interpreter--lang-tag");
+const langIcons = Array.from(document.querySelectorAll(".lang-icon"));
 const icons = {
     es: "spain",
     en: "uk",
@@ -514,15 +515,31 @@ const icons = {
     pt: "portugal"
 };
 const getIconUrl = (langCode)=>`img/${icons[langCode]}.png`;
+function escolherIcones(code) {
+    const nameOrigen = [
+        "en",
+        "jp",
+        "kr"
+    ];
+    if (code !== "ss") toggleIcon(code);
+    else nameOrigen.forEach((namae)=>{
+        console.log(namae);
+        toggleIcon(namae);
+    });
+}
+function toggleIcon(langCode) {
+    langIcons.forEach((icon)=>{
+        if (icon.id === langCode) icon.classList.remove("hidden");
+        else icon.classList.add("hidden");
+    });
+}
 elementos.forEach((elemento)=>{
     elemento.addEventListener("mouseover", (el)=>{
         const lang = el.target.dataset.tran.split("|")[0];
         const tran = el.target.dataset.tran.split("|")[1];
-        const img = document.createElement("img");
         el.target.classList.add("traducido");
         interpreter.innerHTML = tran || "";
-        img.src = new URL(getIconUrl(lang) || "", "file:///script.js");
-        langTag.appendChild(img);
+        escolherIcones(lang);
     }, true);
     elemento.addEventListener("mouseout", (el)=>{
         el.target.classList.remove("traducido");
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const elementos = Array.from(document.querySelector('section').children);
 const interpreter = document.querySelector('.interpreter--text');
 const langTag = document.querySelector('.interpreter--lang-tag');
+const langIcons = Array.from(document.querySelectorAll('.lang-icon'));
 const icons = {
   es: 'spain',
   en: 'uk',
@@ -11,17 +12,35 @@ const icons = {
 
 const getIconUrl = langCode => `img/${icons[langCode]}.png`;
 
+function escolherIcones(code) {
+  const nameOrigen = ['en', 'jp', 'kr'];
+
+  if (code !== 'ss') {
+    toggleIcon(code);
+  } else {
+    nameOrigen.forEach(namae => {
+      console.log(namae)
+      toggleIcon(namae);
+    })
+  }
+}
+
+function toggleIcon(langCode) {
+  langIcons.forEach(icon => {
+    if (icon.id === langCode) icon.classList.remove('hidden');
+    else icon.classList.add('hidden');
+  });
+}
+
 elementos.forEach(elemento => {
   elemento.addEventListener('mouseover', el => {
     const lang = el.target.dataset.tran.split('|')[0];
     const tran = el.target.dataset.tran.split('|')[1];
-    const img = document.createElement('img');
 
     el.target.classList.add('traducido');
     interpreter.innerHTML = tran || '';
-    
-    img.src = new URL(getIconUrl(lang) || '', import.meta.url);
-    langTag.appendChild(img)
+
+    escolherIcones(lang);
   }, true);
 
   elemento.addEventListener('mouseout', el => {
